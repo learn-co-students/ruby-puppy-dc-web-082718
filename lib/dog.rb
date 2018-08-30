3dbbf3356463f18abcdbb1c0ab45a757bcca8ad1
Srikant Kumar Kalaputapu <sriki@Srikants-MacBook-Pro.local>
2018-08-30 15:55:26 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Dog
+
+    attr_reader :name
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+
+        @@all.each do |name|
+            # binding.pry
+            puts name.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+end
